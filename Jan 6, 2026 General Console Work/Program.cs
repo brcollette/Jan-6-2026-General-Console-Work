6b3ec8050c6f758114fb4648b16a5adfd9c05de0
Brandon Collette <brcollette@gmail.com>
2025-01-08 19:44:12 -0700

    General updates

--- Jan 6, 2026 General Console Work/Program.cs
+++ Jan 6, 2026 General Console Work/Program.cs
@@ -14,6 +14,7 @@ class Program
 
     {
         string[] names = new string[5];
+          
         names[0] = "John";
         names[1] = "James";
         names[2] = "Jane";
@@ -28,6 +29,8 @@ class Program
         Console.WriteLine(names[3]);
         Console.WriteLine(names[4]);
 
+        Sort(names);    
+
         int currentYear = DateTime.Now.Year;
         Console.WriteLine(currentYear);
 
@@ -39,7 +42,27 @@ class Program
 
         int currentHour = DateTime.Now.Hour;
         Console.WriteLine(currentHour);
+
+        int currentMinute = DateTime.Now.Minute;
+        Console.WriteLine(currentMinute);
+
+        Console.WriteLine($"The current year, month, day, hour (in 24hr format), and minutes are: \n {currentYear} \n {currentMonth} \n {currentDay} \n {currentHour} \n {currentMinute}");
+
     }
+
+    private static void Sort(string[] names) // YOU NEED TO LEARN WHY YOU DID NOT UNDERSTAND THAT THIS NEEDS TO BE A METHOD FOR SORT TO WORK
+    {
+        Array.Sort(names); // Also need to understand why this "names" does not obtain any reference from the above "names" - like shouldent this one be different?
+        Console.WriteLine($"The sorted array is: {names}"); // GOT THIS FROM INTELLISENSE. NEED TO FIGURE OUT. WHY? + HOW ARE THEY SORTED?
+        Console.WriteLine(names[0]);
+        Console.WriteLine(names[1]);
+        Console.WriteLine(names[2]);
+        Console.WriteLine(names[3]);
+        Console.WriteLine(names[4]);
+    }
+    //{
+    //    Sort(names); // YOu need to figure out what is going on here. Leads to some kind of overflow.
+    //}
 }
 
 //Console.WriteLine("Enter the monthly amount?");
